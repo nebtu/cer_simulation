8a32793ec15ff954c846f537209708a6e1d2c3f0
Benjamin Fallmann <benjamin.fallmann@meduniwien.ac.at>
2025-11-12 16:21:44 +0100

    Make sure n redistribution is identical to original simulation

--- R/cer_sim_example.R
+++ R/cer_sim_example.R
@@ -106,16 +106,16 @@ get_sim_adaption <- function(futility, alt_drop = FALSE) {
     drop_arms <- which(drop_hyp)
 
     new_n <- redistribute_n(
-      n = c(design$n_treatments[1:4], design$n_controls[1]),
+      n = c(design$n_controls[1], design$n_treatments[1:4]),
       t = design$t,
-      which_drop = drop_arms,
+      which_drop = drop_arms + 1,
     )
 
     design |>
       multiarm_drop_arms(
         c(drop_arms, drop_arms + 4),
-        n_cont_2 = rep(new_n[5], 2),
-        n_treat_2 = rep(new_n[1:4], 2),
+        n_cont_2 = rep(new_n[1], 2),
+        n_treat_2 = rep(new_n[2:5], 2),
         alt_adj = alt_drop
       )
   }
